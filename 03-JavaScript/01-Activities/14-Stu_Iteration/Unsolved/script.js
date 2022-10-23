12f06995c8787afd98bb3862b87a2f1e83dddbc8
kyleburnette1872 <114832212+kyleburnette1872@users.noreply.github.com>
2022-10-23 19:40:20 -0400

    modified class files

--- 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,9 @@
 //WRITE YOUR CODE BELOW
+// create a variable with 5 student names
+var students = ["Michael", "Ralph", "Bobby", "Ronnie"];
+
+console.log(students.length);
+
+//student array logged in console
+for (var i=0; i < students.length; i++) { 
+    console.log("Great to see you," + students[i] + "!");
