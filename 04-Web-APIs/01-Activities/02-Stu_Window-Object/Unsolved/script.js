8abb9771736bac7941f975a34fc9286ac0853ea3
kyleburnette1872 <114832212+kyleburnette1872@users.noreply.github.com>
2022-10-26 19:40:53 -0400

    Update files from 10-25-2022

--- 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,15 @@
 // TODO: What will the following line of code log?
 console.log(window);
+// returns a reference to the console object, which provides methods for logging information to the browser's console.
 
 // TODO: What will the following line of code log?
 console.log(window.document);
+// returns a reference to the document contained in the window.
 
 // TODO: What will the following line of code log?
 console.log(document.documentElement);
+// returns the Element that is the root element of the document
 
 // TODO: What will the following line of code log?
 console.log(document.head);
-
+//returns the element of the current document.
