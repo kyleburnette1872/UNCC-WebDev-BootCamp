f864b063769b111132272798bce4c3fd2abfe56a
Shane <shanepurgason.98@gmail.com>
2022-11-13 22:21:04 -0500

    11/10/2022 solutions

--- 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -2,6 +2,8 @@ var badRequestUrl = 'https://api.github.com/unicorns';
 var redirectUrl = './404.html';
 
 fetch(badRequestUrl).then(function (response) {
+
+  console.log(response.status);
   // Use a conditional to check the response status.
   // If that status equals the conditional, then redirect to the 404 page.
 });
