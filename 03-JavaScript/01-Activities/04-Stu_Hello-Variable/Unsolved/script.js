83f1f7ee7af908d69e86b910d7c470cdc939f786
kyleburnette1872 <114832212+kyleburnette1872@users.noreply.github.com>
2022-11-26 14:12:48 -0500

    Merge branch 'main' of https://unchar.bootcampcontent.com/The-Coding-Bootcamp/UNCC-VIRT-FSF-PT-10-2022-U-LOLC

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,6 +1,10 @@
 // Write Your JavaScript Code Here
-var personName = 'Kyle'
-var pets - '0';
-var funFact = 'I have a business'
+var personName = 'Kyle';
+var pets = '0';
+var funFact = 'I have a business';
 
-console
+console.log("My name is"  + personName);
+console.log("I have"  +  pets  +  "pets");
+console.log("My Fun Fact is"  + funFact);
+
+personName = "Ricky" ;
