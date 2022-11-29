8bc91f2806d96fb8f9a04163f33a3d4a8b1bcfa1
kyleburnette1872 <114832212+kyleburnette1872@users.noreply.github.com>
2022-11-28 19:45:19 -0500

    upated files

--- 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
+++ 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
@@ -4,3 +4,17 @@ var expression1 = (x < 25);
 var expression2 = (x > 50);
 
 // Write Your JavaScript Code Here
+//"True ✅ True ✅" is logged when both `expression1` and `expression2` are true.
+if (expression1 && expression2) {
+    console.log("True ✅ True ✅");
+  } 
+  //"True ✅ False ❌" is logged when `expression1` is true.
+  else if (expression1) {
+    console.log("True ✅ False ❌");
+  } //"False ❌ True ✅" is logged when `expression2` is true. 
+  else if (expression2) {
+    console.log("False ❌ True ✅");
+  } //"False ❌ False ❌" is logged when both `expression1` and `expression2` are false.
+  else {
+    console.log("False ❌ False ❌");
+  }
