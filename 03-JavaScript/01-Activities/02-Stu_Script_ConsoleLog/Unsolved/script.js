93329e36b987773d3aa6cbeaae6c6c3a9a84b8d4
kyleburnette1872 <114832212+kyleburnette1872@users.noreply.github.com>
2022-12-05 15:45:50 -0500

    update 02-Stu_Script_ConsoleLog

--- 03-JavaScript/01-Activities/02-Stu_Script_ConsoleLog/Unsolved/script.js
+++ 03-JavaScript/01-Activities/02-Stu_Script_ConsoleLog/Unsolved/script.js
@@ -1,4 +1,4 @@
 // Open up the console to see this log
 console.log("Your external JavaScript file is linked 🎉");
-console.log("Boooo");
-console.log("It sucks");
+console.log("Boooo!");
+console.log("It's terrible!");
