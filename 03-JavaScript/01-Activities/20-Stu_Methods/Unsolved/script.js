a0944814ada0bf827ac4553506e4ce4963ab9d67
kyleburnette1872 <114832212+kyleburnette1872@users.noreply.github.com>
2022-12-05 15:36:38 -0500

    update files

--- 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
+++ 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
@@ -3,3 +3,39 @@ var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 var star = "polaris";
 
 // WRITE YOUR CODE BELOW
+//`"Canis Major"` is added as the first element of the `constellations` array and no elements are removed
+//The correct answer:
+constellations.unshift('Canis Major')
+console.log(constellations)
+//My answer 
+//console.log(constellations);
+//constellations.push('Canis Major');
+//constellations.sort();
+//This only moves Canis Major to the end.
+
+
+//`"Venus"` is removed from the `planets` array.
+//Correct answer
+planets.pop()
+console.log(planets)
+//My answer
+//var planetsSliced = planets.slice(0,5);
+//console.log(planetsSliced);
+
+
+//Arrays `constellations` and `planets` are joined to form a new array named `galaxy`.
+//The arrays `constellations` and `planets` should not be altered.
+//Correct answer:
+var galaxy = constellations.concat(planets);
+console.log(galaxy)
+//My answer
+//var galaxy = (constellations + planets);
+//console.log(galaxy);
+//which isn't an arry method.
+
+//It's done when the string `"polaris"` is converted into all capital letters and the new string is stored in a variable.
+var polarisCapped = star.toUpperCase();
+console.log(polarisCapped)
+//My answer
+//var polarisCapped = star.replace("polaris", "POLARIS");
+//console.log(polarisCapped);
--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,19 @@
 //WRITE YOUR CODE BELOW
+//The `customerOrder` object has three properties that store the drink's name, the number of sugars, and a Boolean indicating whether the order is ready.
+var customerOrder = {
+    drinkName: 'coffee',
+    sugars: 3,
+    isReady: true, //Uncaught SyntaxError: Unexpected identifier 'isReady' (at script.js:6:5)
+}
+//The drink name and the number of sugars is logged to the console.
+console.log(customerOrder.drinkName),
+console.log(customerOrder["sugars"]),
+
+//The order is ready, the message `"Ready for pick-up"` logs.
+if (customerOrder.isReady) {
+    console.log("Order Ready")
+} else { //The order is not ready, the message `"Still in order queue"` logs.
+    console.log("Still In Queue")
+}
+
+
