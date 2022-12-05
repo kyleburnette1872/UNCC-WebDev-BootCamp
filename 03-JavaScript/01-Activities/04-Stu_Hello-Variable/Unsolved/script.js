75d5f11575570330b33377558500983074b3d2d6
kyleburnette1872 <114832212+kyleburnette1872@users.noreply.github.com>
2022-12-05 15:59:14 -0500

    update 0Javascript-4-Stu_Hello-Varible

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,10 +1,13 @@
 // Write Your JavaScript Code Here
-var personName = 'Kyle';
-var pets = '0';
+var personName = ' Kyle';
+var pets = ' 0 ';
 var funFact = 'I have a business';
 
-console.log("My name is"  + personName);
+console.log("My name is"  +  personName);
 console.log("I have"  +  pets  +  "pets");
 console.log("My Fun Fact is"  + funFact);
 
-personName = "Ricky" ;
+personName = " Ricky " ;
+pets = ' 12 '
+funFact = ' I live in the woods! '
+console.log('My name is ' + personName +  '. I have ' + pets + ' pets. My fun fact is that ' + funFact  + ' .' );
