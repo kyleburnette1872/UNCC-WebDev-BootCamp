581eb19d64f66a2c3c8fdf7cef2512926942a7ff
kyleburnette1872 <114832212+kyleburnette1872@users.noreply.github.com>
2022-12-08 18:06:24 -0500

    update Java activities

--- 03-JavaScript/01-Activities/24-Stu_Object-Methods/Unsolved/script.js
+++ 03-JavaScript/01-Activities/24-Stu_Object-Methods/Unsolved/script.js
@@ -10,21 +10,22 @@ var shelter = {
 
 //Debug the code below 
 function dogMessage() {
-   console.log("Congrats! " + chosenPet.Lulu + ", a great dog, is available for adoption!");
-   apptMessage();
+   console.log("Congrats! " + chosenPet.Lulu + ", a great dog, is available for adoption!"); // take out Lulu from "chosenPet.Lulu"
+   apptMessage();// should be shelter.apptMessage
 }
 
 function catMessage() {
   console.log("Congrats! " + chosenPet + ", an awesome cat, is available for adoption!");
-  shelter.apptMessage;
+  shelter.apptMessage; //should be a () after apptMessage
 }
 
-if (dogs.includes(chosenPet)) {
+if (dogs.includes(chosenPet)) { // should be shelter.dogs.includes
   dogMessage();
-} else if (cats.includes(chosenPet)) {
+} else if (cats.includes(chosenPet)) { //should be "shelter.cats.includes"
   catMessage();
 } else {
   console.log("It looks like the pet is not available.");
   console.log("Check out our featured dog, " + shelter[0] + ". or our featured cat, " + cat.shelter[1]);
-}
+}// The above line should be  "shelter.dogs[0]" and "shelter.cats[1]"
+
   
