12be7b08a484dc571b38b0e5abfa23992ec378e8
kyleburnette1872 <114832212+kyleburnette1872@users.noreply.github.com>
2023-03-14 17:47:04 -0400

    update

--- 03-JavaScript/02-Challenge/Develop/script.js
+++ 03-JavaScript/02-Challenge/Develop/script.js
@@ -3,6 +3,27 @@
 THEN I am presented with a series of prompts for password criteria:
 length of the password(8-128 characters); 
 character types (lowercase/uppercase, numeric/special characters */
+
+// Array of options for computer to pick from
+var options = {
+  uppercase: [`A B C D E F G H I J K L M N O P Q R S T U V W X Y Z`],
+  lowercase: [`a b c d e f g h i j k l m n o p q r s t u v w x y z`],
+  numeric: [`0,1,2,3,4,5,6,7,8,9,0`],
+  characters: [`!@#$ %^&*(){}[]=<>/.`],
+};
+
+// Get random index from array of options
+var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", function () {
+  var randomSelect = "";
+  for (var i = 0; i < length; i++) {
+    randomSelect += Math.trunc(Math.random() * options.lowercase.length);
+  }
+  return randomSelect;
+  console.log(randomSelect);
+
+  /*
 var generatePassword = function () {
   var promptWindow = prompt(
     "Select the length of password between 8-128 characters"
@@ -57,3 +78,6 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+*/
+});
